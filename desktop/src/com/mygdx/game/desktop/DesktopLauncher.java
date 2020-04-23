61d716aa74bc0424131388c92ad01f1e1e76aa5e
Youssefanter <51537980+Youssefanter@users.noreply.github.com>
2020-04-24 00:10:49 +0200

    New desktop launcher

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.mygdx.game.MainGame;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
@@ -14,7 +15,10 @@ import org.lwjgl.opengl.DisplayMode;
 public class DesktopLauncher {
 	public static void main(String[] arg) throws LWJGLException {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-	
+		config.foregroundFPS=60;
+		config.width=MainGame.width;
+		config.height=MainGame.height;
+		config.resizable=false;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
